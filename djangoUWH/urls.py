0ae0963892675c3072ee06ad911c463b8d9c2750
Karl Doege <kdoe13@gmail.com>
2025-08-18 16:14:23 -0600

    update db entries correctly from .env file in docker compose

--- djangoUWH/urls.py
+++ djangoUWH/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('attendance/', include('attendance.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# Serve static and media files in DEBUG mode
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
